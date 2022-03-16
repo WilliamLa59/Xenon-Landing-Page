b19e46d6676043bc8a760ad1d20a2767c443381a
William La <william.la59@gmail.com>
2022-03-16 13:00:15 -0400

    Hero complete
    - colors, font and font sizes not 1:1
    (but will change once graphics come in)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,34 @@ import React from 'react'
 export const Hero = () => {
   return (
     <div className='hero-container'>
-        <p>Improving Lives Together</p>
+        <div className='hero-header-wrapper'>
+            <div className='hero-header-container'>
+                <div className='hero-header-text'>
+                    <h1>Improving</h1>
+                    <h1>Lives</h1>
+                    <h1>Together</h1>
+                </div>
+            </div>
+        </div>
+        <div className='hero-text'>
+            <h1>Welcome to your Xenon (team 11) Method</h1>
+            <h1>Our application is designed to give support to health care workers ANYWHERE</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+            
+            <div className='hero-text-btn'>
+                <button>Find Out More</button>
+            </div>
+        </div>
     </div>
   )
 }
